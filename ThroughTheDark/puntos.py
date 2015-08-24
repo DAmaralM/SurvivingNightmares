e93943383907df24141c13d66f6ecaa489a36f1a
usuario <usuario@ai>
2015-08-24 19:24:48 -0300

    Hoy hicimos los sonidos y los puntos

---
+++ ThroughTheDark/puntos.py
@@ -0,0 +1,36 @@
+import pygame
+
+from funciones_spritesheet import *
+
+# Estas contantes definen el tipo de plataforma.
+#   Nombre del archivo.
+#   Posicion X del sprite
+#   Posicion Y del sprite
+#   Ancho del sprite
+#   Alto del sprite
+
+ESTRELLA          = (0, 0, 17, 16)
+
+#Hoy hicimos los sonidos y los puntos
+
+class Estrellas(pygame.sprite.Sprite):
+    """ Clase que define las caracteristicas de la plataforma del juego. """
+
+    def __init__(self, sprite_sheet_data):
+        """ Plataforma constructor."""
+        pygame.sprite.Sprite.__init__(self)
+        sprite_sheet = SpriteSheetNegro("imagenes/puntos.png")
+        self.sonido_comida = pygame.mixer.Sound("sonidos/agarrar_puntos.ogg")
+        
+        # Grab the image for this platform
+        self.image = sprite_sheet.obtener_imagen(sprite_sheet_data[0],
+                                            sprite_sheet_data[1],
+                                            sprite_sheet_data[2],
+                                            sprite_sheet_data[3])
+
+        self.rect = self.image.get_rect()
+    def hacer_sonido(self):
+        self.sonido_comida.play()
+        
+
+
